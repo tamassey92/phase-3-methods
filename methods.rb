be4f7d38ae6d97272e14ba2b705a2f6cb75a9c9b
Taylor Massey <tamassey92@gmail.com>
2022-05-16 10:11:49 -0600

    commit tree 674cb253ac18d98345baa2a42820591eaac0ccac

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default
+
+greet_with_default "Sunny"
+
+def add(num1, num2)
+    return num1 + num2
+end
+sum = add(1,2)
+
+def halve(number) 
+    return number / 2
+end
+
+halve(4)
+halve("name")
